1be88142f2f713728926ccf8c69c6a5ef7776304
Seltmann <enicospilus@gmail.com>
2020-05-12 17:08:31 -0700

    new repo for site

---
+++ Index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+
+<!--
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+-->
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" src="./css/custom.css">
+
+<title>Virtual Center for the Study of Biotic Interactions (ViCSBI)</title>
+<meta name="description" content="Virtual Center for the Study of Biotic Interactions (ViCSBI)">
+<meta name="author" content="Katja Seltmann">
+</head>
+
+<body>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<!--
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+-->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+<div class="container">
+<h1>Virtual Center for the Study of Biotic Interactions (ViCSBI)</h1>
+</div>
+<div class="container">
+<p>This area can have the description of the the Virtual Center for the Study of Biotic Interactions (ViCSBI)</p>
+</div>
+<?php //phpinfo(); ?>
+</body>
+
+</html>
